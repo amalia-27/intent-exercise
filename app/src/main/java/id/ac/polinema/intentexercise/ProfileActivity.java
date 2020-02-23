ffc0693ce46e291927b0cb415a8f46e3f9b9967d
amali <amalia.hartini27@gmail.com>
2020-02-23 11:26:15 +0700

    Mengerjakan Tugas

--- app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
@@ -2,13 +2,51 @@ package id.ac.polinema.intentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+    private TextView aboutText;
+    private TextView nameText;
+    private TextView emailText;
+    private TextView homePageText;
+    private ImageView avatarImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        aboutText = findViewById(R.id.label_about);
+        nameText = findViewById(R.id.label_fullname);
+        emailText = findViewById(R.id.label_email);
+        homePageText = findViewById(R.id.label_homepage);
+        avatarImage = findViewById(R.id.image_profile);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+
+            // TODO: display value here
+            Bundle extra = getIntent().getExtras();
+            Bitmap bmp = extra.getParcelable("image");
+            avatarImage.setImageBitmap(bmp);
+
+            aboutText.setText(extras.getString("about"));
+            nameText.setText(extras.getString("fullname"));
+            emailText.setText(extras.getString("email"));
+            homePageText.setText(extras.getString("homepage"));
+        }
+    }
+    public void handleHomePage(View view) {
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String homepageText = bundle.getString("homepage");
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + homepageText));
+             startActivity(intent);
+        }
     }
 }
--- app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
@@ -1,14 +1,115 @@
 package id.ac.polinema.intentexercise;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = RegisterActivity.class.getCanonicalName();
+    private static final int GALLERY_REQUEST_CODE = 1;
+
+    private ImageView avatarImage;
+
+    public static final String FULLNAME_KEY = "fullname";
+    public static final String EMAIL_KEY = "email";
+    public static final String PASSWORD_KEY = "password";
+    public static final String CONFIRM_KEY = "confirm";
+    public static final String HOMEPAGE_KEY = "homepage";
+    public static final String ABOUT_KEY = "about";
+    public static final String AVATAR_KEY = "image";
+
+    private EditText fullnameInput;
+    private EditText emailInput;
+    private EditText passwordInput;
+    private EditText confirmInput;
+    private EditText homepageInput;
+    private EditText aboutInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        avatarImage = findViewById(R.id.image_profile);
+        fullnameInput = findViewById(R.id.text_fullname);
+        emailInput = findViewById(R.id.text_email);
+        passwordInput = findViewById(R.id.text_password);
+        confirmInput = findViewById(R.id.text_confirm_password);
+        homepageInput = findViewById(R.id.text_homepage);
+        aboutInput = findViewById(R.id.text_about);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if (data != null) {
+                try {
+                    Uri imageUri = data.getData();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    avatarImage.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    Toast.makeText(this, "Can't load image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }
+    }
+
+    public void changeAvatar(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+    }
+
+    public void handleOK(View view) {
+        String fullname = fullnameInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        String confirm = confirmInput.getText().toString();
+        String homepage = homepageInput.getText().toString();
+        String about = aboutInput.getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if ((fullname).equals("") || (email).equals("") || (password).equals("") || (confirm).equals("") || (homepage).equals("") || (about).equals("")) {
+            Toast.makeText(this, "Data tidak boleh kosong", Toast.LENGTH_SHORT).show();
+        }
+        else if(!password.equals(confirm)) {
+            Toast.makeText(this, "Password tidak sesuai", Toast.LENGTH_SHORT).show();
+        }
+        else if (!email.matches(emailPattern)) {
+            Toast.makeText(this, "Email tidak valid", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent intent = new Intent(this, ProfileActivity.class);
+
+            avatarImage.buildDrawingCache();
+            Bitmap image = avatarImage.getDrawingCache();
+            Bundle extras = new Bundle();
+            extras.putParcelable(AVATAR_KEY, image);
+            intent.putExtras(extras);
+
+            intent.putExtra(FULLNAME_KEY, fullname);
+            intent.putExtra(EMAIL_KEY, email);
+            intent.putExtra(PASSWORD_KEY, password);
+            intent.putExtra(CONFIRM_KEY, confirm);
+            intent.putExtra(HOMEPAGE_KEY, homepage);
+            intent.putExtra(ABOUT_KEY, about);
+
+            startActivity(intent);
+        }
     }
 }
